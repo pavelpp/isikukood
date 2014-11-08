72cdc7de774da46a8ab4cad1fc1646d29354ea23
pavelpp <pavelponomaryov@gmail.com>
2014-11-08 10:53:10 +0200

    added  subprocessing with pbcopy

--- isikukood/isikukood.py
+++ isikukood/isikukood.py
@@ -1,22 +1,20 @@
 __author__ = 'pavel'
 
 from random import randint
+import subprocess
 
 def generate():
     """generates a valid estonian personal code"""
-    s_cent = randint(3, 6)
-    serial_nr = randint(0, 999)
+    s_cent = randint(1, 6)
+    serial_nr = randint(1, 999)
     code = str(s_cent) + gen_date() + zeroes(serial_nr, 3)
     final_code = code + control_nr(code)
-
     return final_code
 
 def gen_date():
     """generates a valid date"""
     byear = randint(0, 99)
     bmonth = randint(1, 12)
-
-
     if bmonth == 2 and is_leap(byear):
         bday = randint(1, 29)
     elif bmonth == 2:
@@ -28,6 +26,18 @@ def gen_date():
     return zeroes(byear, 2) + zeroes(bmonth, 2) + zeroes(bday, 2)
 
 
+def get_bday(month, year):
+    if month == 2 and is_leap(year):
+        bday = randint(1, 29)
+    elif month == 2:
+        bday = randint(1, 28)
+    elif month in [4, 6, 9, 11]:
+        bday = randint(1, 30)
+    else:
+        bday = randint(1, 31)
+    return bday
+
+
 def is_leap(year):
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
         return True
@@ -35,13 +45,10 @@ def is_leap(year):
 
 def control_nr(code):
     code = [int(i) for i in code]
-
     weight1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
     weight2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
-
     sum1 = sum([x*y for x,y in zip(code, weight1)])
     sum2 = sum([x*y for x,y in zip(code, weight2)])
-
     if sum1 % 11 != 10:
         return str(sum1 % 11)
     elif sum2 % 11 != 10:
@@ -59,4 +66,7 @@ def run_times(times):
     for n in range(times):
         print(generate())
 
-run_times(10)
+if __name__ == "__main__":
+    isik = generate().strip("")
+    cmd = 'echo \"%s\" | pbcopy' % isik
+    subprocess.check_output(cmd, shell=True)
